0e3374a58d40430e6cad212d35cc72cc84d4486b
fancybit <fancybit@qq.com>
2025-12-08 01:49:24 +0800

    chore: 更新程序集信息缓存和版本号

--- metajade-csharp/dotnet/MetaJadeBridge/obj/Debug/net9.0/MetaJadeBridge.AssemblyInfo.cs
+++ metajade-csharp/dotnet/MetaJadeBridge/obj/Debug/net9.0/MetaJadeBridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MetaJadeBridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8d874403cfac05cf452ca0656279fd7d75d995")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e67e8e11a96bc0bcff0e193a24a5ee624fd6ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("MetaJadeBridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MetaJadeBridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
