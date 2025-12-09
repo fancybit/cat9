b235ac98cdd99bf59534461b149bacc3a261d94d
fancybit <fancybit@qq.com>
2025-12-09 09:11:04 +0800

    移除metajade-csharp目录，准备添加为git子模块

--- metajade-csharp/JadeClient/GlobalXmlns.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace JadeClient
-{
-    public static class GlobalXmlns
-    {
-        // Intentionally empty - used for XAML xml:namespace mappings
-    }
-}
--- metajade-csharp/LibMetaJade/Docs/TOC.md
+++
@@ -1,7 +0,0 @@
-# Table of Contents
-
-- README_IMPLEMENTATION.md
-- API.md
-- README.md
-
-Add other docs here as needed.
